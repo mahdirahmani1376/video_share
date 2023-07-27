20f29cebb95e0fb88d090227183215a839fbedfa
mahdirahmani1376 <rahmanimahdi20@yahoo.com>
2023-07-27 19:10:11 +0330

    first_commit

---
+++ app/Http/Controllers/VideosController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideosController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Video $video)
+    {
+    }
+
+    public function edit(Video $video)
+    {
+    }
+
+    public function update(Request $request, Video $video)
+    {
+    }
+
+    public function destroy(Video $video)
+    {
+    }
+}
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/bootstrap.min.css',
+                'resources/css/responsive.css',
+                'resources/css/style.css',
+                // 'resources/css/grid-blog.min.js',
+                // 'resources/css/imagesloaded.pkgd.min.js',
+                // 'resources/css/jquery.sticky-kit.min.js',
+                // 'resources/css/jquery-3.2.1.min.js',
+                // 'resources/css/smooth-scroll.min.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
+
